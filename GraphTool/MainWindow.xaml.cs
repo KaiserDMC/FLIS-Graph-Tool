3ebfc15b840dc5f16657b0c9b5de83acb1bfec28
Krisztian Fodor <kris.fodor66@gmail.com>
2023-11-14 21:21:28 +0100

    initial-transition-wpf-2

---
+++ GraphTool/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace GraphTool;
+
+using System.Windows;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
